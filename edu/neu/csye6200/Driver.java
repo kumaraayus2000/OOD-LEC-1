0ae019473099e1360d63784016d76e33806828df
aayushkumar <kumar.aayus@northeastern.edu>
2024-09-12 11:49:06 -0400

    ood_assignment_1

---
+++ edu/neu/csye6200/Driver.java
@@ -0,0 +1,17 @@
+package edu.neu.csye6200;
+
+import static edu.neu.csye6200.Driver.Sample.demo;
+
+public class Driver {
+
+    public class Sample{
+        public static void demo(){
+            System.out.println("Hello World");
+        }
+    }
+
+    public static void main(String[] args){
+        demo();
+    }
+
+}
